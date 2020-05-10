dba52bf9a1f594bf38e5025d877ba089255dae79
Amal Nair <amalrocks29@gmail.com>
2020-05-11 03:30:25 +0530

    update app.js

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ function onclick() {
   newbgtext2 = "#" + (Math.random*0xFFFFF<<0).toString(16);
   newbgtext3 = "#" + (Math.random*0xFFFFF<<0).toString(16)
   
+  if(newbg1<7 || newbg2<7||newbgtext3<7||newbgtext1<7||newbgtext2<7)
+    onclick();
+  
   bg.style.background = linear-gradient(-45deg, newbg1, newbg2);
   
   textbg.style.background = linear-gradient(to right,newbgtext1, newbgtext2, newbgtext3);
