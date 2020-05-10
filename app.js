25b9ead463e5d76e6db5e4e98b11a125186f5ee5
Amal Nair <amalrocks29@gmail.com>
2020-05-11 03:09:38 +0530

    update app.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 window.body.addEventListener("click", onclick);
+var bg = document.getElementById("bg");
+var textbg = document.getElementById("textbg");
 
 function onclick() {
   document.body.getElementById('bg').classList;
