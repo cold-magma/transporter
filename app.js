e921baca105374d0049162df28c46c260dbee166
Amal Nair <amalrocks29@gmail.com>
2020-05-11 09:43:55 +0530

    update app.js

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ var textbg = document.getElementById("textbg")
 document.body.addEventListener("click", onclick);
 
 function onclick() {
-  newbg1 = "#" + (Math.random*0xFFFFF<<0).toString(16);
- newbg2 = "#" + (Math.random*0xFFFFF<<0).toString(16);
-  newbgtext1 = "#" + (Math.random*0xFFFFF<<0).toString(16);
-  newbgtext2 = "#" + (Math.random*0xFFFFF<<0).toString(16);
-  newbgtext3 = "#" + (Math.random*0xFFFFF<<0).toString(16)
+  newbg1 = "#" + (Math.random*0xFFFFFF<<0).toString(16);
+ newbg2 = "#" + (Math.random*0xFFFFFF<<0).toString(16);
+  newbgtext1 = "#" + (Math.random*0xFFFFFF<<0).toString(16);
+  newbgtext2 = "#" + (Math.random*0xFFFFFF<<0).toString(16);
+  newbgtext3 = "#" + (Math.random*0xFFFFFF<<0).toString(16)
   
-  if(newbg1<7 || newbg2<7||newbgtext3<7||newbgtext1<7||newbgtext2<7)
+  if(newbg1.length<7 || newbg2.length<7||newbgtext3.length<7||newbgtext1.length<7||newbgtext2.length<7)
     onclick();
   
   bg.style.background = newbg1;
