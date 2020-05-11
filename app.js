f8a4aeb806e0bb9fd5212a5540747c5d9f2b5b80
Amal Nair <amalrocks29@gmail.com>
2020-05-11 09:48:31 +0530

    update app.js

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ var textbg = document.getElementById("textbg")
 document.body.addEventListener("click", onclick);
 
 function onclick() {
-  newbg1 = "#" + (Math.random*0xFFFFFF<<0).toString(16);
- newbg2 = "#" + (Math.random*0xFFFFFF<<0).toString(16);
-  newbgtext1 = "#" + (Math.random*0xFFFFFF<<0).toString(16);
-  newbgtext2 = "#" + (Math.random*0xFFFFFF<<0).toString(16);
-  newbgtext3 = "#" + (Math.random*0xFFFFFF<<0).toString(16)
+  newbg1 = "#" + (Math.random()*0xFFFFFF<<0).toString(16);
+  newbg2 = "#" + (Math.random()*0xFFFFFF<<0).toString(16);
+  newbgtext1 = "#" + (Math.random()*0xFFFFFF<<0).toString(16);
+  newbgtext2 = "#" + (Math.random()*0xFFFFFF<<0).toString(16);
+  newbgtext3 = "#" + (Math.random()*0xFFFFFF<<0).toString(16)
   
   if(newbg1.length<7 || newbg2.length<7||newbgtext3.length<7||newbgtext1.length<7||newbgtext2.length<7)
     onclick();
