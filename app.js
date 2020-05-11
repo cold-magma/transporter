89de519e718403aaca578d15c1b3e269cc945707
Amal Nair <amalrocks29@gmail.com>
2020-05-11 20:48:27 +0530

    update app.js

--- app.js
+++ app.js
@@ -10,11 +10,10 @@ function onclick() {
   newbg2 = "#" + (Math.random()*0xFFFFFF<<0).toString(16);
   newbgtext1 = "#" + (Math.random()*0xFFFFFF<<0).toString(16);
   newbgtext2 = "#" + (Math.random()*0xFFFFFF<<0).toString(16);
-  newbgtext3 = "#" + (Math.random()*0xFFFFFF<<0).toString(16)
   
-  if(newbg1.length<7 || newbg2.length<7||newbgtext3.length<7||newbgtext1.length<7||newbgtext2.length<7)
+  if(newbg1.length<7 || newbg2.length<7||newbgtext1.length<7||newbgtext2.length<7)
     onclick();
-   if(newbg1 == newbg2 || newbgtext2==newbgtext1 || newbgtext1==newbgtext3 || newbgtext2==newbgtext3 || newbg1 == newbgtext3 || newbg1 == newbgtext1 || newbg1 == newbgtext2 || newbg2 == newbgtext1 || newbg2 == newbgtext2 || newbg2 == newbgtext3)
+   if(newbg1 == newbg2 || newbgtext2==newbgtext1 || || newbg1 == newbgtext1 || newbg1 == newbgtext2 || newbg2 == newbgtext1 || newbg2 == newbgtext2)
     onclick();
  
   inv.style.opacity="1";
@@ -23,7 +22,7 @@ function onclick() {
     inv.style.opacity="0";
     bg.style.backgroundSize = "200% 200%";
     bg.style.backgroundRepeat = "no-repeat";
-    textbg.style.background = "linear-gradient(-45deg," + newbgtext1 +"," + newbgtext2 + "," + newbgtext3 + ")";
+    textbg.style.background = "linear-gradient(-45deg," + newbgtext1 +"," + newbgtext2 + "," + newbgtext1 + ")";
     textbg.style.backgroundSize = "200% 200%";
     textbg.style.backgroundRepeat = "no-repeat";
     textbg.style.webkitBackgroundClip = "text";
